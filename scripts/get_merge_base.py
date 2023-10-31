27470a4098824ec6eacd53a74d6ea2f9f68888a6
Miguel Ángel Mireles Vázquez <mamv3x3@gmail.com>
2023-10-31 14:22:51 -0600

    feat(get_merge_base): improved base acquisition for cinder

---
+++ scripts/get_merge_base.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+import sys
+from bench_runner import git
+from bench_runner.result import has_result
+
+def main(need_to_run: bool, machine: str, pystats: bool, cinder: Path = Path("cinder")) -> None:
+    if not need_to_run:
+        print("ref=xxxxxxx")
+        print("need_to_run=false")
+    else:
+        merge_base = git.get_git_merge_base(cinder)
+
+        if merge_base is None:
+            print("ref=xxxxxxx")
+            print("need_to_run=false")
+        else:
+            need_to_run = (
+                machine == "all"
+                or has_result(Path("results"), merge_base, machine, pystats) is None
+            )
+
+            print(f"ref={merge_base}")
+            print(f"need_to_run={str(need_to_run).lower()}")
+
+if __name__ == "__main__":
+    need_to_run = sys.argv[-3] != "false"
+    pystats = sys.argv[-1] != "false"
+
+    main(need_to_run, sys.argv[-2], pystats)
